ef83bac69d40fe2dcfa1d96687a0b922a381d4e6
gianmarco.iapoce <root@DESKTOP-2MMDGLC.inventia.biz>
2020-12-22 17:19:02 +0100

    fix

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -4,26 +4,20 @@ module "eks" {
   cluster_version = "1.17"
   subnets         = module.vpc.private_subnets
 
-  tags = {
-    Environment = "gitlab-test"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
-  }
-
   vpc_id = module.vpc.vpc_id
 
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
+      instance_type                 = "t3.small"
+      additional_userdata           = "echo test"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
       name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
+      instance_type                 = "t3.medium"
+      additional_userdata           = "echo test"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
